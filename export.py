8b13470d9cf89191a53d5a24685d95fccb0b7c94
drw <drw@users.noreply.github.com>
2018-10-16 00:54:55 -0400

    Automate renaming of cycled-out file in export process

--- export.py
+++ export.py
@@ -1,6 +1,8 @@
 import os, dataset, datafreeze
 
 filename = "/Users/drw/code/talespin/lines.db"
+alternate_filename = "/Users/drw/code/talespin/l.db"
+assert filename != alternate_filename
 table_name = 'talespin_lines'
 
 
@@ -11,3 +13,4 @@ if not db_exists:
 db = dataset.connect('sqlite:///{}'.format(filename))
 result = db[table_name].all()
 datafreeze.freeze(result, format='csv', filename='quotes.csv')
+os.rename(filename,alternate_filename)
