ce09109a04a201378843efe5f47c78fe6a604f11
drw <drw@users.noreply.github.com>
2019-08-25 15:01:22 -0400

    Modify export.py to import db_filename from tell.py

--- export.py
+++ export.py
@@ -1,16 +1,16 @@
 import os, dataset, datafreeze
 
-filename = "/Users/drw/code/talespin/lines.sqlite"
+from tell import db_filename
 alternate_filename = "/Users/drw/code/talespin/l.sqlite"
-assert filename != alternate_filename
+assert db_filename != alternate_filename
 table_name = 'talespin_lines'
 
 
 import os.path
-db_exists = os.path.isfile(filename)
+db_exists = os.path.isfile(db_filename)
 if not db_exists:
     raise ValueError("No such database exists.")
-db = dataset.connect('sqlite:///{}'.format(filename))
+db = dataset.connect('sqlite:///{}'.format(db_filename))
 result = db[table_name].all()
 datafreeze.freeze(result, format='csv', filename='quotes.csv')
-os.rename(filename,alternate_filename)
+os.rename(db_filename,alternate_filename)
